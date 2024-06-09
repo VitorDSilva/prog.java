95de16f1c34f3990ee47f483fe74164139e14b78
VitorDSilva <vitordiogo.csilva@gmail.com>
2024-06-09 16:19:38 +0100

    Update Carta.java
    Comentários

--- modelo/cartas/Carta.java
+++ modelo/cartas/Carta.java
@@ -2,17 +2,17 @@ package modelo.cartas;
 
 import modelo.enums.DescricaoDeCarta;
 
-/**
-
- */
 public abstract class Carta {
 
+	// Variável de instância para armazenar a descrição da carta
     private final DescricaoDeCarta descricao;
 
+	// Construtor que inicializa a descrição da carta
     public Carta(DescricaoDeCarta descricao) {
         this.descricao = descricao;
     }
 
+	// Retorna a descrição
     public DescricaoDeCarta getDescricao() {
 		return descricao;
 	}
